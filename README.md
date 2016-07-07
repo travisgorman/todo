66c0ddc1e805fb52dff90a107aca19d45228732d
Travis Gorman <thefarbetween@me.com>
2016-07-07 10:14:49 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # todo
+
+![](http://i.imgur.com/UQ2Fy9w.png)
+![](http://i.imgur.com/YxXfyJ7.png)
--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log( 'hello, world' );
+$.ajax({
+  url: 'http://tiny-za-server.herokuapp.com/collections/tgToDoList',
+  dataType: 'json',
+  type: 'POST',
+  data: {
+    "todo": "make a container"
+  },
+  success: function( response ){
+    console.log( response );
+    
+  } 
+});
+
+$.ajax({
+  url: "http://tiny-za-server.herokuapp.com/collections/tgToDoList",
+  dataType: "json",
+  type: "DELETE",
+  data: {
+    "_id": "577d773b69837d0300d191b2"
+  },
+  success: function( response ){
+    console.log( response )
+  }
+});
+
+
