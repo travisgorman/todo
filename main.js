edc35e9e300376cc090c1f301121d95f37e7bf21
Travis Gorman <thefarbetween@me.com>
2016-07-07 23:15:20 -0500

    re-structured for making elements with javascript

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
 $.ajax({
   url: 'http://tiny-za-server.herokuapp.com/collections/tgToDoList',
   dataType: 'json',
-  type: 'POST',
-  data: {
-    "todo": "make a container"
-  },
+  type: 'GET',
   success: function( response ){
     console.log( response );
     
   } 
 });
-
-$.ajax({
-  url: "http://tiny-za-server.herokuapp.com/collections/tgToDoList",
-  dataType: "json",
-  type: "DELETE",
-  data: {
-    "_id": "577d773b69837d0300d191b2"
-  },
-  success: function( response ){
-    console.log( response )
-  }
-});
-
-
